db5fbde8f4178bf1353c859479d4f6534d2a0d73
LoganColey <lcoley22@basecampcodingacademy.org>
2021-11-15 13:16:36 -0600

    Create admin.py

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Character)
+admin.site.register(Monster)
+admin.site.register(Comment)
+admin.site.register(Campaign)
+admin.site.register(StatsC)
+admin.site.register(StatsM)
+admin.site.register(Notes)
