1a7814b71bae556d8640877d007c1a601354e3d9
LoganColey <lcoley22@basecampcodingacademy.org>
2021-11-17 11:08:18 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -38,12 +38,13 @@ urlpatterns = [
     path("search_monster/", views.search_monster, name="search-m"),
     path("search_character/", views.search_character, name="search-c"),
     path("search_campaign/", views.search_campaign, name="search-ca"),
-    path("add_stats_m/<str:pk>/", views.add_mstats_view, name="m_stats")
-    
-
-
-    # path('welcome/' views.welcome_page, name='welcome'),
-    # path('adminview/' views.admin_view, name="admin"),
+    path("add_stats_m/<str:pk>/", views.add_mstats_view, name="m_stats"),
+    path("add_stats_c/<str:pk>/", views.add_cstats_view, name="c_stats"),
+    path("add_notes/<str:pk>/", views.add_notes_view, name="notes"),
+    path('view_monster/<str:pk>/', views.view_monster, name='view_monster'),
+    path('view_character/<str:pk>/', views.view_character, name='view_character'),
+    path('view_campaign/<str:pk>/', views.view_campaign, name='view_campaign'),
+    path('random_character/',views.random_characters, name='randomch')
    
 
 ]
